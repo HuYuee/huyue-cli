90c613b4542f3fe979847050e829f0b6cfb9a6aa
huyueb <1062887235@qq.com>
2017-04-10 22:15:23 +0800

    初始化

--- bin/hy.js
+++ bin/hy.js
@@ -10,22 +10,6 @@ program
     .usage('<command>')
     .description('输入不同的姓名称号，来获取来自huyue不同的评价吧！')
 
-program
-    .command('zhangchengming')
-    .alias('zcm')
-    .description('查看他对张诚铭的评价')
-    .action(() => {
-        require('../command/zcm')()
-    })
-program
-    .command('linwei')
-    .alias('lw')
-    .description('查看他对林维的评价')
-    .action(() => {
-        require('../command/lw')()
-    })
-program.parse(process.argv)
-
 if (!program.args.length) {
     program.help()
 }
